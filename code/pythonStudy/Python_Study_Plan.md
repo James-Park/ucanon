701242fb143d9e9c2736f887a1df6cddd177671e
Park Sang Kyu <pskpassion@gmail.com>
2021-04-04 22:57:05 +0900

    Python Study Plan

---
+++ code/pythonStudy/Python_Study_Plan.md
@@ -0,0 +1,36 @@
+# Python Study Plan
+
+* Python 시작하기
+  - [ ] Python 설치 및 개발 환경 구축
+  - [ ] Python 'Hello World' 출력 프로그램 작성 및 실행
+* Phython 기초 문법
+  - [ ] 변수명과 예약어
+  - [ ] 자료형과 연산자
+  - [ ] 객체
+  - [ ] 제어문
+  - [ ] 함수
+  - [ ] 입출력
+
+- Python 고급 문법
+  - [ ] 모듈
+  - [ ] 클래스
+  - [ ] 예외처리
+- Python 네트워크 프로그래밍
+  - [ ] Echo Server / Client 작성
+  - [ ] Simple Web Server 작성
+  - [ ] Chatting Server / Client 작성
+- Python 웹 프로그래밍
+  - [ ] Phython 웹표준 라이브러리
+  - [ ] Django 설치
+  - [ ] MVC 패턴 프로그램 작성
+  - [ ] MySQL 연동하기
+  - [ ] 회원 정보 조회 및 등록/수정/삭제 프로그래밍 작성 (HTML 코드 서버 생성 방식)
+  - [ ] 회원 정보 조회 및 등록/수정/삭제 프로그래밍 작성 (JSON 포맷으로 Ajax 통신 방식)
+- Python 응용 프로그래밍
+  - [ ] BeautifulSoup을 활용한 HTML 파싱
+  - [ ] Tkinter 활용해서 미니 계산기 만들기
+  - [ ] 공공 API를 활용한 크롤러 작성하기
+  - [ ] Selenium 라이브러리 활용한 동적 웹페이지 크롤링
+- Python 라이브러리를 활용한 데이터 분석
+  - [ ] 자연어처리 라이브러리 활용(nltk, KoNLpy)
+  - [ ] 데이터처리 라이브러리 활용(NumPy, Pandas)
---
+++ index.js
@@ -0,0 +1,66 @@
+function init() {
+    var mdPath = getParameter("mp");
+    if (mdPath !== "") {
+        loadWebPage(mdPath, loadViewer);
+    }
+    setMenuShow();
+}
+
+function loadViewer(content) {
+    const Viewer = toastui.Editor;
+    const codeSyntaxHighlight = Viewer.plugin.codeSyntaxHighlight;
+    const viewer = new Viewer({
+        el: document.querySelector("#viewer"),
+        initialValue: content,
+        plugins: [codeSyntaxHighlight],
+    });
+}
+
+function loadWebPage(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+
+    xhr.responseType = "text";
+
+    xhr.onload = function () {
+        if (xhr.readyState === xhr.DONE && xhr.status === 200) {
+            callback(xhr.responseText);
+        }
+    };
+
+    xhr.send();
+}
+
+function getParameter(param) {
+    var url = decodeURI(location.href);
+    var paramArr = url
+        .substring(url.indexOf("#") + 1, url.length)
+        .split("&");
+    var value = "";
+
+    for (var i = 0; i < paramArr.length; i++) {
+        var temp = paramArr[i].split("=");
+
+        if (temp[0].toUpperCase() == param.toUpperCase()) {
+            value = paramArr[i].split("=")[1];
+            break;
+        }
+    }
+
+    return value;
+}
+
+function goPage(url) {
+    window.location.href = "#mp=" + encodeURI(url);
+    loadWebPage(url, loadViewer);
+}
+
+function setMenuShow() {
+    $(".left_menu .main").click(function () {
+        if ($(this).parent().find(".sub").is(":visible")) {
+            $(this).parent().find(".sub").slideUp(100);
+        } else {
+            $(this).parent().find(".sub").slideDown(100);
+        }
+    })
+}
