972bb6f5e7ea04892d5c461cab5df508677c0756
James, Park <pskpassion@gmail.com>
2025-09-11 09:10:16 +0900

    Mac 한글 파일명 복원 PowerShell 웹 브라우저 기능을 활요한 프런트앤드 오류 수집 전략

---
+++ code/PowerShell/Mac한글파일명복원/MacFileNameConversion.ps1
@@ -0,0 +1,12 @@
+﻿$path = "C:\Users\canon\Downloads\Mac파일명복원"
+
+Get-ChildItem -Recurse -Path $path | Where-Object { $_.Extension -ne ".ps1" } | ForEach-Object {
+    $oldName = $_.Name
+    $newName = $oldName.Normalize([Text.NormalizationForm]::FormC)
+    if ($oldName -ne $newName) {
+        $oldFull = $_.FullName
+        $newFull = Join-Path $_.DirectoryName $newName
+        Rename-Item -LiteralPath $oldFull -NewName $newName
+        Write-Host "변환 완료: $oldName → $newName"
+    }
+}
